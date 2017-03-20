7dda08af34cd394ebe3c4759080d8a9784fa03d4
Rebecca <Rebecca@172.20.94.41>
2017-03-20 09:55:08 +0100

    Classe esame

---
+++ src/it/polito/tdp/libretto/model/TestEsame.java
@@ -0,0 +1,25 @@
+package it.polito.tdp.libretto.model;
+
+import java.time.LocalDate;
+
+public class TestEsame {
+
+	public static void main(String[] args) {
+		
+		Esame tdp = new Esame("03FYZ", "Tecniche di Programmazione", "Fulvio Corno");
+		System.out.println(tdp);
+		
+		Esame ami = new Esame("01QZP", "Ambient Intelligence", "Fulvio Corno");
+		System.out.println(ami);
+		System.out.println(ami.equals(tdp));
+		
+		tdp.supera(30, LocalDate.now());
+		System.out.println(tdp);
+		
+		tdp.supera(18, LocalDate.now());
+		System.out.println(tdp);
+		
+
+	}
+
+}
